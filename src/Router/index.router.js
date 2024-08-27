f6f2b8afa0b45416f14974b90bf748467194d197
harishraajaa <rvharishraajaa@gmail.com>
2024-08-27 22:36:55 +0530

    Fixed Bug

--- src/Router/index.router.js
+++ src/Router/index.router.js
@@ -4,7 +4,7 @@ import studentrouter from './student.router.js'
 
  
 const router=express.Router()
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     res.send("<div style='text-align:center><h2>Student & Mentor Assigning using MongoDB Database</h2></div>");
   });
 router.use('/students',studentrouter)
