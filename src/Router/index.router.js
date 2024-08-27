4a9c015b7e6b568a9ed24952dd5d7d01844ab087
harishraajaa <rvharishraajaa@gmail.com>
2024-08-27 22:40:33 +0530

    Updated HomePage

--- src/Router/index.router.js
+++ src/Router/index.router.js
@@ -5,8 +5,8 @@ import studentrouter from './student.router.js'
  
 const router=express.Router()
 router.get("/", (req, res) => {
-    res.send("<div style='text-align:center><h2>Student & Mentor Assigning using MongoDB Database</h2></div>");
-  });
+    res.send(`<div style='text-align:center'><h2>Student & Mentor Assigning using MongoDB Database</h2></div>`)
+  })
 router.use('/students',studentrouter)
 router.use('/mentors',mentorrouter)
 router.get('/*',(request,response)=>response.send(`<div style='text-align:center'><h1>404 Endpoint Not found</h1></div`))
